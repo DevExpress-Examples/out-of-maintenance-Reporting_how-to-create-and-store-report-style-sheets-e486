d27c1ae0cbf2c1e5e153a6130029ea23061e8a2b
DevExpressExampleBot <fakeEmail>
2019-08-29 17:34:52 +0300

    Source auto update [skip ci]

---
+++ VB/XtraReport1.Designer.vb
@@ -0,0 +1,176 @@
+﻿Namespace DynamicStyleSheet
+	Partial Public Class XtraReport1
+		''' <summary>
+		''' Required designer variable.
+		''' </summary>
+		Private components As System.ComponentModel.IContainer = Nothing
+
+		''' <summary> 
+		''' Clean up any resources being used.
+		''' </summary>
+		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+			If disposing AndAlso (components IsNot Nothing) Then
+				components.Dispose()
+			End If
+			MyBase.Dispose(disposing)
+		End Sub
+
+		#Region "Designer generated code"
+
+		''' <summary>
+		''' Required method for Designer support - do not modify
+		''' the contents of this method with the code editor.
+		''' </summary>
+		Private Sub InitializeComponent()
+			Me.components = New System.ComponentModel.Container()
+			Dim selectQuery1 As New DevExpress.DataAccess.Sql.SelectQuery()
+			Dim column1 As New DevExpress.DataAccess.Sql.Column()
+			Dim columnExpression1 As New DevExpress.DataAccess.Sql.ColumnExpression()
+			Dim table1 As New DevExpress.DataAccess.Sql.Table()
+			Dim column2 As New DevExpress.DataAccess.Sql.Column()
+			Dim columnExpression2 As New DevExpress.DataAccess.Sql.ColumnExpression()
+			Dim column3 As New DevExpress.DataAccess.Sql.Column()
+			Dim columnExpression3 As New DevExpress.DataAccess.Sql.ColumnExpression()
+			Dim resources As New System.ComponentModel.ComponentResourceManager(GetType(XtraReport1))
+			Me.Detail = New DevExpress.XtraReports.UI.DetailBand()
+			Me.xrTable1 = New DevExpress.XtraReports.UI.XRTable()
+			Me.xrTableRow1 = New DevExpress.XtraReports.UI.XRTableRow()
+			Me.xrTableCell1 = New DevExpress.XtraReports.UI.XRTableCell()
+			Me.xrTableCell2 = New DevExpress.XtraReports.UI.XRTableCell()
+			Me.xrTableCell3 = New DevExpress.XtraReports.UI.XRTableCell()
+			Me.PageHeader = New DevExpress.XtraReports.UI.PageHeaderBand()
+			Me.PageFooter = New DevExpress.XtraReports.UI.PageFooterBand()
+			Me.myOddStyle = New DevExpress.XtraReports.UI.XRControlStyle()
+			Me.topMarginBand1 = New DevExpress.XtraReports.UI.TopMarginBand()
+			Me.bottomMarginBand1 = New DevExpress.XtraReports.UI.BottomMarginBand()
+			Me.sqlDataSource1 = New DevExpress.DataAccess.Sql.SqlDataSource(Me.components)
+			CType(Me.xrTable1, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(Me, System.ComponentModel.ISupportInitialize).BeginInit()
+			' 
+			' Detail
+			' 
+			Me.Detail.Controls.AddRange(New DevExpress.XtraReports.UI.XRControl() { Me.xrTable1})
+			Me.Detail.HeightF = 42F
+			Me.Detail.Name = "Detail"
+			Me.Detail.Padding = New DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F)
+			Me.Detail.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft
+			' 
+			' xrTable1
+			' 
+			Me.xrTable1.LocationFloat = New DevExpress.Utils.PointFloat(17F, 17F)
+			Me.xrTable1.Name = "xrTable1"
+			Me.xrTable1.OddStyleName = "myOddStyle"
+			Me.xrTable1.Rows.AddRange(New DevExpress.XtraReports.UI.XRTableRow() { Me.xrTableRow1})
+			Me.xrTable1.SizeF = New System.Drawing.SizeF(333F, 25F)
+			' 
+			' xrTableRow1
+			' 
+			Me.xrTableRow1.Cells.AddRange(New DevExpress.XtraReports.UI.XRTableCell() { Me.xrTableCell1, Me.xrTableCell2, Me.xrTableCell3})
+			Me.xrTableRow1.Name = "xrTableRow1"
+			Me.xrTableRow1.Weight = 1R
+			' 
+			' xrTableCell1
+			' 
+			Me.xrTableCell1.ExpressionBindings.AddRange(New DevExpress.XtraReports.UI.ExpressionBinding() { New DevExpress.XtraReports.UI.ExpressionBinding("BeforePrint", "Text", "[ProductID]")})
+			Me.xrTableCell1.Name = "xrTableCell1"
+			Me.xrTableCell1.Padding = New DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 100F)
+			Me.xrTableCell1.Text = "xrTableCell1"
+			Me.xrTableCell1.Weight = 0.24924924924924924R
+			' 
+			' xrTableCell2
+			' 
+			Me.xrTableCell2.ExpressionBindings.AddRange(New DevExpress.XtraReports.UI.ExpressionBinding() { New DevExpress.XtraReports.UI.ExpressionBinding("BeforePrint", "Text", "[ProductName]")})
+			Me.xrTableCell2.Name = "xrTableCell2"
+			Me.xrTableCell2.Padding = New DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 100F)
+			Me.xrTableCell2.Text = "xrTableCell2"
+			Me.xrTableCell2.Weight = 0.47447447447447449R
+			' 
+			' xrTableCell3
+			' 
+			Me.xrTableCell3.ExpressionBindings.AddRange(New DevExpress.XtraReports.UI.ExpressionBinding() { New DevExpress.XtraReports.UI.ExpressionBinding("BeforePrint", "Text", "[UnitPrice]")})
+			Me.xrTableCell3.Name = "xrTableCell3"
+			Me.xrTableCell3.Padding = New DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 100F)
+			Me.xrTableCell3.Text = "xrTableCell3"
+			Me.xrTableCell3.Weight = 0.27627627627627627R
+			' 
+			' PageHeader
+			' 
+			Me.PageHeader.HeightF = 30F
+			Me.PageHeader.Name = "PageHeader"
+			Me.PageHeader.Padding = New DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F)
+			Me.PageHeader.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft
+			' 
+			' PageFooter
+			' 
+			Me.PageFooter.HeightF = 30F
+			Me.PageFooter.Name = "PageFooter"
+			Me.PageFooter.Padding = New DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F)
+			Me.PageFooter.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft
+			' 
+			' myOddStyle
+			' 
+			Me.myOddStyle.BackColor = System.Drawing.Color.AliceBlue
+			Me.myOddStyle.Name = "myOddStyle"
+			' 
+			' topMarginBand1
+			' 
+			Me.topMarginBand1.HeightF = 100F
+			Me.topMarginBand1.Name = "topMarginBand1"
+			' 
+			' bottomMarginBand1
+			' 
+			Me.bottomMarginBand1.HeightF = 100F
+			Me.bottomMarginBand1.Name = "bottomMarginBand1"
+			' 
+			' sqlDataSource1
+			' 
+			Me.sqlDataSource1.ConnectionName = "DynamicStyleSheet.Properties.Settings.nwindConnectionString1"
+			Me.sqlDataSource1.Name = "sqlDataSource1"
+			columnExpression1.ColumnName = "ProductID"
+			table1.Name = "Products"
+			columnExpression1.Table = table1
+			column1.Expression = columnExpression1
+			columnExpression2.ColumnName = "ProductName"
+			columnExpression2.Table = table1
+			column2.Expression = columnExpression2
+			columnExpression3.ColumnName = "UnitPrice"
+			columnExpression3.Table = table1
+			column3.Expression = columnExpression3
+			selectQuery1.Columns.Add(column1)
+			selectQuery1.Columns.Add(column2)
+			selectQuery1.Columns.Add(column3)
+			selectQuery1.Name = "Products"
+			selectQuery1.Tables.Add(table1)
+			Me.sqlDataSource1.Queries.AddRange(New DevExpress.DataAccess.Sql.SqlQuery() { selectQuery1})
+			Me.sqlDataSource1.ResultSchemaSerializable = resources.GetString("sqlDataSource1.ResultSchemaSerializable")
+			' 
+			' XtraReport1
+			' 
+			Me.Bands.AddRange(New DevExpress.XtraReports.UI.Band() { Me.Detail, Me.PageHeader, Me.PageFooter, Me.topMarginBand1, Me.bottomMarginBand1})
+			Me.ComponentStorage.AddRange(New System.ComponentModel.IComponent() { Me.sqlDataSource1})
+			Me.DataMember = "Products"
+			Me.DataSource = Me.sqlDataSource1
+			Me.StyleSheet.AddRange(New DevExpress.XtraReports.UI.XRControlStyle() { Me.myOddStyle})
+			Me.Version = "17.2"
+			CType(Me.xrTable1, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(Me, System.ComponentModel.ISupportInitialize).EndInit()
+
+		End Sub
+
+		#End Region
+
+		Private Detail As DevExpress.XtraReports.UI.DetailBand
+		Private PageHeader As DevExpress.XtraReports.UI.PageHeaderBand
+		Private PageFooter As DevExpress.XtraReports.UI.PageFooterBand
+		Private xrTableRow1 As DevExpress.XtraReports.UI.XRTableRow
+		Private xrTableCell1 As DevExpress.XtraReports.UI.XRTableCell
+		Private xrTableCell2 As DevExpress.XtraReports.UI.XRTableCell
+		Private xrTableCell3 As DevExpress.XtraReports.UI.XRTableCell
+		Public xrTable1 As DevExpress.XtraReports.UI.XRTable
+		Private myOddStyle As DevExpress.XtraReports.UI.XRControlStyle
+		Private topMarginBand1 As DevExpress.XtraReports.UI.TopMarginBand
+		Private bottomMarginBand1 As DevExpress.XtraReports.UI.BottomMarginBand
+		Private sqlDataSource1 As DevExpress.DataAccess.Sql.SqlDataSource
+	End Class
+End Namespace
