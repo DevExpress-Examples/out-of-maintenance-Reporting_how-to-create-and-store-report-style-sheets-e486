c521d752c822a2e54fced2d0a8cd3e4ce7b00477
ilya komarov <ilya.komarov@devexpress.com>
2019-08-29 17:48:02 +0300

    Revert "Update to expressions, remove dataset"
    This reverts commit fe3aa6b2f80f8ad6f9185420f4f184fc805eff46.

---
+++ VB/XtraReport1.Designer.vb
@@ -0,0 +1,148 @@
+Imports Microsoft.VisualBasic
+Imports System
+Namespace DynamicStyleSheet
+	Partial Public Class XtraReport1
+		''' <summary>
+		''' Required designer variable.
+		''' </summary>
+		Private components As System.ComponentModel.IContainer = Nothing
+
+		''' <summary> 
+		''' Clean up any resources being used.
+		''' </summary>
+		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+			If disposing AndAlso (components IsNot Nothing) Then
+				components.Dispose()
+			End If
+			MyBase.Dispose(disposing)
+		End Sub
+
+		#Region "Designer generated code"
+
+		''' <summary>
+		''' Required method for Designer support - do not modify
+		''' the contents of this method with the code editor.
+		''' </summary>
+		Private Sub InitializeComponent()
+			Me.Detail = New DevExpress.XtraReports.UI.DetailBand()
+			Me.xrTable1 = New DevExpress.XtraReports.UI.XRTable()
+			Me.xrTableRow1 = New DevExpress.XtraReports.UI.XRTableRow()
+			Me.xrTableCell1 = New DevExpress.XtraReports.UI.XRTableCell()
+			Me.xrTableCell2 = New DevExpress.XtraReports.UI.XRTableCell()
+			Me.xrTableCell3 = New DevExpress.XtraReports.UI.XRTableCell()
+			Me.PageHeader = New DevExpress.XtraReports.UI.PageHeaderBand()
+			Me.PageFooter = New DevExpress.XtraReports.UI.PageFooterBand()
+			Me.myOddStyle = New DevExpress.XtraReports.UI.XRControlStyle()
+			Me.nwindDataSet1 = New DynamicStyleSheet.nwindDataSet()
+			Me.productsTableAdapter = New DynamicStyleSheet.nwindDataSetTableAdapters.ProductsTableAdapter()
+			CType(Me.xrTable1, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(Me.nwindDataSet1, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(Me, System.ComponentModel.ISupportInitialize).BeginInit()
+			' 
+			' Detail
+			' 
+			Me.Detail.Controls.AddRange(New DevExpress.XtraReports.UI.XRControl() { Me.xrTable1})
+			Me.Detail.Height = 42
+			Me.Detail.Name = "Detail"
+			Me.Detail.Padding = New DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F)
+			Me.Detail.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft
+			' 
+			' xrTable1
+			' 
+			Me.xrTable1.Location = New System.Drawing.Point(17, 17)
+			Me.xrTable1.Name = "xrTable1"
+			Me.xrTable1.OddStyleName = "myOddStyle"
+			Me.xrTable1.Rows.AddRange(New DevExpress.XtraReports.UI.XRTableRow() { Me.xrTableRow1})
+			Me.xrTable1.Size = New System.Drawing.Size(333, 25)
+			' 
+			' xrTableRow1
+			' 
+			Me.xrTableRow1.Cells.AddRange(New DevExpress.XtraReports.UI.XRTableCell() { Me.xrTableCell1, Me.xrTableCell2, Me.xrTableCell3})
+			Me.xrTableRow1.Name = "xrTableRow1"
+			Me.xrTableRow1.Size = New System.Drawing.Size(333, 25)
+			' 
+			' xrTableCell1
+			' 
+			Me.xrTableCell1.DataBindings.AddRange(New DevExpress.XtraReports.UI.XRBinding() { New DevExpress.XtraReports.UI.XRBinding("Text", Nothing, "Products.ProductID", "")})
+			Me.xrTableCell1.Location = New System.Drawing.Point(0, 0)
+			Me.xrTableCell1.Name = "xrTableCell1"
+			Me.xrTableCell1.Padding = New DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 100F)
+			Me.xrTableCell1.Size = New System.Drawing.Size(83, 25)
+			Me.xrTableCell1.Text = "xrTableCell1"
+			' 
+			' xrTableCell2
+			' 
+			Me.xrTableCell2.DataBindings.AddRange(New DevExpress.XtraReports.UI.XRBinding() { New DevExpress.XtraReports.UI.XRBinding("Text", Nothing, "Products.ProductName", "")})
+			Me.xrTableCell2.Location = New System.Drawing.Point(83, 0)
+			Me.xrTableCell2.Name = "xrTableCell2"
+			Me.xrTableCell2.Padding = New DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 100F)
+			Me.xrTableCell2.Size = New System.Drawing.Size(158, 25)
+			Me.xrTableCell2.Text = "xrTableCell2"
+			' 
+			' xrTableCell3
+			' 
+			Me.xrTableCell3.DataBindings.AddRange(New DevExpress.XtraReports.UI.XRBinding() { New DevExpress.XtraReports.UI.XRBinding("Text", Nothing, "Products.UnitPrice", "")})
+			Me.xrTableCell3.Location = New System.Drawing.Point(241, 0)
+			Me.xrTableCell3.Name = "xrTableCell3"
+			Me.xrTableCell3.Padding = New DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 100F)
+			Me.xrTableCell3.Size = New System.Drawing.Size(92, 25)
+			Me.xrTableCell3.Text = "xrTableCell3"
+			' 
+			' PageHeader
+			' 
+			Me.PageHeader.Height = 30
+			Me.PageHeader.Name = "PageHeader"
+			Me.PageHeader.Padding = New DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F)
+			Me.PageHeader.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft
+			' 
+			' PageFooter
+			' 
+			Me.PageFooter.Height = 30
+			Me.PageFooter.Name = "PageFooter"
+			Me.PageFooter.Padding = New DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 100F)
+			Me.PageFooter.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft
+			' 
+			' myOddStyle
+			' 
+			Me.myOddStyle.BackColor = System.Drawing.Color.AliceBlue
+			Me.myOddStyle.Name = "myOddStyle"
+			' 
+			' nwindDataSet1
+			' 
+			Me.nwindDataSet1.DataSetName = "nwindDataSet"
+			Me.nwindDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+			' 
+			' productsTableAdapter
+			' 
+			Me.productsTableAdapter.ClearBeforeFill = True
+			' 
+			' XtraReport1
+			' 
+			Me.Bands.AddRange(New DevExpress.XtraReports.UI.Band() { Me.Detail, Me.PageHeader, Me.PageFooter})
+			Me.DataAdapter = Me.productsTableAdapter
+			Me.DataMember = "Products"
+			Me.DataSource = Me.nwindDataSet1
+			Me.StyleSheet.AddRange(New DevExpress.XtraReports.UI.XRControlStyle() { Me.myOddStyle})
+			Me.Version = "8.2"
+			CType(Me.xrTable1, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(Me.nwindDataSet1, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(Me, System.ComponentModel.ISupportInitialize).EndInit()
+
+		End Sub
+
+		#End Region
+
+		Private Detail As DevExpress.XtraReports.UI.DetailBand
+		Private PageHeader As DevExpress.XtraReports.UI.PageHeaderBand
+		Private PageFooter As DevExpress.XtraReports.UI.PageFooterBand
+		Private xrTableRow1 As DevExpress.XtraReports.UI.XRTableRow
+		Private xrTableCell1 As DevExpress.XtraReports.UI.XRTableCell
+		Private xrTableCell2 As DevExpress.XtraReports.UI.XRTableCell
+		Private xrTableCell3 As DevExpress.XtraReports.UI.XRTableCell
+		Public xrTable1 As DevExpress.XtraReports.UI.XRTable
+		Private myOddStyle As DevExpress.XtraReports.UI.XRControlStyle
+		Private nwindDataSet1 As nwindDataSet
+		Private productsTableAdapter As DynamicStyleSheet.nwindDataSetTableAdapters.ProductsTableAdapter
+	End Class
+End Namespace
